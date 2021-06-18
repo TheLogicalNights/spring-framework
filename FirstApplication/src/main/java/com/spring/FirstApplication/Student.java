23780a6409d3efcd3f1f63281e210134b8d374e3
TheLogicalNights <swapniladhav560@gmail.com>
2021-06-18 17:27:47 +0530

    Updated student setter methods

--- FirstApplication/src/main/java/com/spring/FirstApplication/Student.java
+++ FirstApplication/src/main/java/com/spring/FirstApplication/Student.java
@@ -10,18 +10,21 @@ public class Student
 		return studentId;
 	}
 	public void setStudentId(int studentId) {
+		System.out.println("Setting student id");
 		this.studentId = studentId;
 	}
 	public String getStudentName() {
 		return studentName;
 	}
 	public void setStudentName(String studentName) {
+		System.out.println("Setting student name");
 		this.studentName = studentName;
 	}
 	public String getStudentAddress() {
 		return studentAddress;
 	}
 	public void setStudentAddress(String studentAddress) {
+		System.out.println("Setting student address");
 		this.studentAddress = studentAddress;
 	}
 	@Override
