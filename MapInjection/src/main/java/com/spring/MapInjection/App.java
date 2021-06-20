bd58f02a859eb5b914109ec7535c0d2db5be699e
TheLogicalNights <swapniladhav560@gmail.com>
2021-06-20 12:10:32 +0530

    Added Map Injection

---
+++ MapInjection/src/main/java/com/spring/MapInjection/App.java
@@ -0,0 +1,21 @@
+package com.spring.MapInjection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Student student = (Student) context.getBean("student");
+        
+        System.out.println(student.getStudentRollNo());
+        System.out.println(student.getStudentName());
+        System.out.println(student.getCourse());
+    }
+}
---
+++ MapInjection/src/main/java/com/spring/MapInjection/Student.java
@@ -0,0 +1,29 @@
+package com.spring.MapInjection;
+
+import java.util.*;
+
+public class Student 
+{
+	private int studentRollNo;
+	private String studentName;
+	private Map<String , String> course;
+	public int getStudentRollNo() {
+		return studentRollNo;
+	}
+	public void setStudentRollNo(int studentRollNo) {
+		this.studentRollNo = studentRollNo;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public Map<String, String> getCourse() {
+		return course;
+	}
+	public void setCourse(Map<String, String> course) {
+		this.course = course;
+	}
+	
+}
