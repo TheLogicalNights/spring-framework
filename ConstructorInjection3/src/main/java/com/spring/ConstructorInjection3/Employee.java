d16428c329517a9711a37f1eabd1dfa43e825388
TheLogicalNights <swapniladhav560@gmail.com>
2021-06-22 12:05:55 +0530

    Added constructor injection using c-schema

---
+++ ConstructorInjection3/src/main/java/com/spring/ConstructorInjection3/Employee.java
@@ -0,0 +1,20 @@
+package com.spring.ConstructorInjection3;
+
+public class Employee 
+{
+	private int empId;
+	private String empName;
+	private double empSalary;
+	
+	public Employee(int empId, String empName, double empSalary) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.empSalary = empSalary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSalary=" + empSalary + "]";
+	}
+}
