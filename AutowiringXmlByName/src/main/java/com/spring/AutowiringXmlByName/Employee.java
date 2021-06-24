29b434d43444bc7b0d0004d6bc6904916b87bbb1
TheLogicalNights <swapniladhav560@gmail.com>
2021-06-24 11:10:06 +0530

    Added Autowiring using xml byName

---
+++ AutowiringXmlByName/src/main/java/com/spring/AutowiringXmlByName/Employee.java
@@ -0,0 +1,20 @@
+package com.spring.AutowiringXmlByName;
+
+public class Employee 
+{
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [address=" + address + "]";
+	}
+	
+}
